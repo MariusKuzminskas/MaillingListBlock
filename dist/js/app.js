41f9c5a88f7e21c86e673c522ffeaed4e72f054c
mariuskuzminskas <kuzimnskas.marius7@gmail.com>
2020-07-23 21:32:05 +0300

    Javascript logic to get an input, validate  and send post requiest using axios. Added feedback for users on invalid input.

---
+++ dist/js/app.js
@@ -0,0 +1,49 @@
+// getting elements 
+const inputEl = document.getElementById('input');
+const formEl = document.querySelector('.form-wrapper__form');
+const errorEl = document.querySelector('.form-wrapper__msg--error');
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+    const inputValue = inputEl.value;
+    if (inputValue && inputValue != 0) {
+        if (emailIsValid(inputValue)) {
+            sendPostRequest(inputValue);
+        } else {
+            errorEl.innerText = "Please check your email";
+        }
+    } else {
+        errorEl.innerText = "Please enter an email";
+    }
+})
+
+
+// functions
+
+function emailIsValid(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+
+
+
+function sendPostRequest(validEmail) {
+    axios.post('https://api.staging.fourthwall.com/api/mailing-list', {
+            email: validEmail
+        }, {
+            headers: {
+                'X-ShopId': 'sh_9f57832f-456b-44f3-888f-8a370b155a18',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(function(response) {
+            console.log(response.status);
+            if (response.status > 200 && response.status < 300) {
+                inputEl.value = '';
+                errorEl.innerText = 'Thank you. Your email has been sent';
+                errorEl.classList.add('form-wrapper__msg--success');
+            }
+        })
+        .catch(function(error) {
+            errorEl.innerText = "Error, something went wrong. Please try again later";
+        });
+}
--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ function watch() {
     });
     gulp.watch('./src/scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./dist/js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
